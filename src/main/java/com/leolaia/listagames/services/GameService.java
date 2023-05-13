c9a558050d78d4fab119c7d886e0c19f22d9e007
Leo Laia <leonardolribo@gmail.com>
2023-05-13 18:04:16 -0300

    Implementação do serviço getByid

--- src/main/java/com/leolaia/listagames/services/GameService.java
+++ src/main/java/com/leolaia/listagames/services/GameService.java
@@ -1,19 +1,29 @@
 package com.leolaia.listagames.services;
 
 
+import com.leolaia.listagames.dto.GameDto;
 import com.leolaia.listagames.dto.GameMinDto;
 import com.leolaia.listagames.entities.Game;
 import com.leolaia.listagames.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+
+    @Transactional(readOnly = true)
+    public GameDto findById(Long id){
+        Game game = gameRepository.findById(id).get();
+        return new GameDto(game);
+    }
+    @Transactional(readOnly = true)
     public List<GameMinDto> findAll(){
         List<Game> games = gameRepository.findAll();
         return games.stream().map(x -> new GameMinDto(x)).toList();
