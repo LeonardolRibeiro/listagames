bc708275f33e6eff0fc1c0d54d6709ca15616551
Leo Laia <leonardolribo@gmail.com>
2023-05-14 19:21:35 -0300

    Reposição da ordem dos games nas listas

--- src/main/java/com/leolaia/listagames/controllers/GameListController.java
+++ src/main/java/com/leolaia/listagames/controllers/GameListController.java
@@ -2,13 +2,11 @@ package com.leolaia.listagames.controllers;
 
 import com.leolaia.listagames.dto.GameListDto;
 import com.leolaia.listagames.dto.GameMinDto;
+import com.leolaia.listagames.dto.ReplacementDto;
 import com.leolaia.listagames.services.GameListService;
 import com.leolaia.listagames.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +26,10 @@ public class GameListController {
     @GetMapping(value = "/{listId}/games")
     public List<GameMinDto> findByList(@PathVariable Long listId){
         return gameService.findByList(listId);
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void replace(@PathVariable Long listId, @RequestBody ReplacementDto body){
+        gameListService.replace(listId, body.getSourceIndex(), body.getDestinationIndex());
     }
 }
